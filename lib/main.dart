883ed4c9a800ab699386f39f6eb6ca699fca9b91
Yury Kozyrev <ustarts@gmail.com>
2021-03-16 18:18:04 +0100

    update everything

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,9 @@ import 'package:flutter/material.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter/foundation.dart' show kDebugMode;
 
 void main() {
-  Crashlytics.instance.enableInDevMode =
-      true; // Pass all uncaught errors from the framework to Crashlytics.
-  FlutterError.onError = Crashlytics.instance.recordFlutterError;
   WidgetsFlutterBinding.ensureInitialized();
   runApp(FirebaseInitializer());
 }
@@ -23,7 +21,13 @@ class FirebaseInitializer extends StatelessWidget {
   Widget build(BuildContext context) {
     return FutureBuilder(
       // Initialize FlutterFire
-      future: Firebase.initializeApp(),
+      future: Firebase.initializeApp().then((snapshot) {
+        if (kDebugMode) {
+          FirebaseCrashlytics.instance
+              .setCrashlyticsCollectionEnabled(false);
+        }
+        return snapshot;
+      }),
       builder: (context, snapshot) {
         // Check for errors
         if (snapshot.hasError) {
