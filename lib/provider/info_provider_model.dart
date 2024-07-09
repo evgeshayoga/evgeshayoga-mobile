8a2c6662dfaaaf5653b73f0e396536bbb1c5c058
Yury Kozyrev <urakozz@me.com>
2024-07-09 16:52:42 +0200

    upgrade everything

--- lib/provider/info_provider_model.dart
+++ lib/provider/info_provider_model.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/foundation.dart';
-import 'package:package_info/package_info.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class InfoProviderModel extends ChangeNotifier {
   bool _initialized = false;
